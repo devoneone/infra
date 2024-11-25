4f16f75f2379d27284d7e554cca7f4d18e53336d
ruos-sovanra <vannraruos@gmail.com>
2024-11-25 11:38:34 +0700

    infra version 1

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,7 +1,4 @@
-def call(String imageName, String imageTag, String credentialsId) {
-    withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
-        sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
-        sh "docker push ${imageName}:${imageTag}"
-        sh "docker logout"
-    }
+def call(String imageName, String imageTag) {
+    sh "docker push ${imageName}:${imageTag}"
+    sh "docker logout"
 }   

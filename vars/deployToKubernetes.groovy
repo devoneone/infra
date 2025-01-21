366923ae3f32b5b35df1f07adc06c1ee0ef3cbed
jessiebrownleo <sobengg5@gmail.com>
2025-01-21 16:25:42 +0700

    add angular Dockerfile v3

--- vars/deployToKubernetes.groovy
+++ vars/deployToKubernetes.groovy
@@ -1,6 +1,5 @@
-
 def call(String inventoryFile, String playbookFile, String appName, String image, 
-         String namespace, String filePath, String domainName, String email, String gitRepoUrl , String branch) {
+         String namespace, String filePath, String domainName, String email, String gitRepoUrl, String branch) {
     
     def tmpDir = "tmp-${appName}-${UUID.randomUUID().toString()}"
     
@@ -9,8 +8,8 @@ def call(String inventoryFile, String playbookFile, String appName, String image
         dir(tmpDir) {
             git(
                 url: gitRepoUrl,
-                branch: branch,  // or specify branch as parameter
-                credentialsId: 'git-credentials'  // specify your credentials ID
+                branch: branch,
+                credentialsId: 'git-credentials'
             )
         }
         
@@ -70,72 +69,61 @@ def validateParameters(String inventoryFile, String playbookFile, String appName
 }
 
 def detectProjectType(String projectPath = '.') {
-    echo "Checking for angular.json in ${projectPath}"
-    if (fileExists("${projectPath}/angular.json")) {
-        echo "Angular project detected through angular.json, setting port to 4200"
-        return [type: 'angular', port: 4200]
-    }
-    else if (fileExists("${projectPath}/artisan")) {
-        echo "Laravel project detected"
-        return [type: 'laravel', port: 8000]
-    }
-    else if (fileExists("${projectPath}/package.json")) {
-        def packageJson = readJSON file: "${projectPath}/package.json"
-        echo "package.json contents: ${packageJson}"
-        
-
-        if (packageJson.dependencies?.next || packageJson.devDependencies?.next) {
-            echo "Next.js project detected, setting port to 3000"
-            return [type: 'nextjs', port: 3000]
-        } else if (packageJson.dependencies?.react || packageJson.devDependencies?.react) {
-            if (packageJson.dependencies?.vite || packageJson.devDependencies?.vite) {
-                echo "React Vite project detected, setting port to 80"
-                return [type: 'vite-react', port: 80]
-            } else {
-                echo "React project detected, setting port to 80"
-                return [type: 'react', port: 80]
+    try {
+        echo "Checking for angular.json in ${projectPath}"
+        if (fileExists("${projectPath}/angular.json")) {
+            echo "Angular project detected through angular.json, setting port to 4200"
+            return [type: 'angular', port: 4200]
+        } else if (fileExists("${projectPath}/artisan")) {
+            echo "Laravel project detected"
+            return [type: 'laravel', port: 8000]
+        } else if (fileExists("${projectPath}/package.json")) {
+            def packageJson = readJSON file: "${projectPath}/package.json"
+            echo "package.json contents: ${packageJson}"
+            
+            if (packageJson.dependencies?.next || packageJson.devDependencies?.next) {
+                echo "Next.js project detected, setting port to 3000"
+                return [type: 'nextjs', port: 3000]
+            } else if (packageJson.dependencies?.react || packageJson.devDependencies?.react) {
+                if (packageJson.dependencies?.vite || packageJson.devDependencies?.vite) {
+                    echo "React Vite project detected, setting port to 80"
+                    return [type: 'vite-react', port: 80]
+                } else {
+                    echo "React project detected, setting port to 80"
+                    return [type: 'react', port: 80]
+                }
+            } else if (packageJson.dependencies?.express || packageJson.devDependencies?.express) {
+                echo "Express project detected, setting port to 3000"
+                return [type: 'express', port: 3000]
+            } else if (packageJson.dependencies?.nuxt || packageJson.devDependencies?.nuxt) {
+                echo "Nuxt.js project detected, setting port to 3000"
+                return [type: 'nuxtjs', port: 80]
+            } else if (packageJson.dependencies?.vue || packageJson.devDependencies?.vue) {
+                echo "Vue.js project detected, setting port to 8080"
+                return [type: 'vuejs', port: 80]
+            } else if (packageJson.dependencies?.svelte || packageJson.devDependencies?.svelte) {
+                echo "Svelte project detected, setting port to 5000"
+                return [type: 'svelte', port: 5000]
+            } else if (packageJson.dependencies?.nestjs || packageJson.devDependencies?.nestjs) {
+                echo "NestJS project detected, setting port to 3000"
+                return [type: 'nestjs', port: 3000]
             }
+        } else if (fileExists("${projectPath}/index.html")) {
+            echo "HTML project detected"
+            return [type: 'html', port: 80]
+        } else if (fileExists("${projectPath}/index.php")) {
+            echo "PHP project detected"
+            return [type: 'php']
+        } else if (fileExists("${projectPath}/pom.xml")) {
+            echo "Spring Boot (Maven) project detected, setting port to 8080"
+            return [type: 'springboot-maven', port: 8080]
+        } else if (fileExists("${projectPath}/build.gradle") || fileExists("${projectPath}/build.gradle.kts")) {
+            echo "Spring Boot (Gradle) project detected, setting port to 8080"
+            return [type: 'springboot-gradle', port: 8080]
+        } else if (fileExists("${projectPath}/pubspec.yaml")) {
+            echo "Flutter project detected, setting port to 8080"
+            return [type: 'flutter', port: 8080]
         }
-
-        else if (packageJson.dependencies?.express || packageJson.devDependencies?.express) {
-            echo "Express project detected, setting port to 3000"
-            return [type: 'express', port: 3000]
-        
-        }else if (packageJson.dependencies?.nuxt || packageJson.devDependencies?.nuxt) {
-            echo "Nuxt.js project detected, setting port to 3000"
-            return [type: 'nuxtjs', port: 80]
-        }else if (packageJson.dependencies?.vue || packageJson.devDependencies?.vue) {
-            echo "Vue.js project detected, setting port to 8080"
-            return [type: 'vuejs', port: 80]
-        }else if (packageJson.dependencies?.svelte || packageJson.devDependencies?.svelte) {
-            echo "Svelte project detected, setting port to 5000"
-            return [type: 'svelte', port: 5000]
-        } else if (packageJson.dependencies?.express || packageJson.devDependencies?.express) {
-            echo "Express project detected, setting port to 3000"
-            return [type: 'express', port: 3000]
-        } else if (packageJson.dependencies?.nestjs || packageJson.devDependencies?.nestjs) {
-            echo "NestJS project detected, setting port to 3000"
-            return [type: 'nestjs', port: 3000]
-        }
-    }else if (fileExists("${projectPath}/index.html")) {
-        echo "HTML project detected"
-        return [type: 'html', port: 80]
-    } else if (fileExists("${projectPath}/index.php")) {
-        echo "PHP project detected"
-        return [type: 'php']
-    } 
-
-    //Detecting Backend Projects
-    else if (fileExists("${projectPath}/pom.xml")) {
-        echo "Spring Boot (Maven) project detected, setting port to 8080"
-        return [type: 'springboot-maven', port: 8080]
-    } else if (fileExists("${projectPath}/build.gradle") || fileExists("${projectPath}/build.gradle.kts")) {
-        echo "Spring Boot (Gradle) project detected, setting port to 8080"
-        return [type: 'springboot-gradle', port: 8080]
-    } else if (fileExists("${projectPath}/pubspec.yaml")) {
-        echo "Flutter project detected, setting port to 8080"
-        return [type: 'flutter', port: 8080]
-    }
         echo "No specific project type detected, using default configuration"
         return [type: 'unknown', port: 8080]
     } catch (Exception e) {
